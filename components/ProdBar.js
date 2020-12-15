a6499d627399094fa7906af58311a468cf64bbd0
Dan Pastusek <dan@hexial.com>
2020-12-14 23:39:41 -0700

    styling

--- components/ProdBar.js
+++ components/ProdBar.js
@@ -1,29 +1,38 @@
 import styles from './ProdBar.module.css'
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const ProdBar = props => {
-
-    const router = useRouter();
+  const router = useRouter()
 
   return (
-
     <div className={styles.ProdBar}>
+      <div>
         <Link href="/box1">
-          <a className={router.pathname === '/box1' ? 'active' : 'inactive'}>Box 1</a>
+          <a className={router.pathname === '/box1' ? 'active' : 'inactive'}>
+            Box <span className={styles.colorPink}>2</span>
+          </a>
         </Link>
         <Link href="/box2">
-          <a className={router.pathname === '/box2' ? 'active' : 'inactive'}>Box 2</a>
+          <a className={router.pathname === '/box2' ? 'active' : 'inactive'}>
+            Box <span className={styles.colorPink}>2 mini</span>
+          </a>
         </Link>
+      </div>
+      <div>
         <Link href="/box3">
-          <a className={`${router.pathname == '/box3' ? `active` : 'inactive'}`}>Box 3</a>
+          <a className={`${router.pathname == '/box3' ? `active` : 'inactive'}`}>
+            Box <span className={styles.colorBlue}>5</span>
+          </a>
         </Link>
         <Link href="/box4">
-          <a className={router.pathname == '/box4' ? 'active' : 'inactive'}>Box 4</a>
+          <a className={router.pathname == '/box4' ? 'active' : 'inactive'}>
+            Box <span className={styles.colorBlue}>5 mini</span>
+          </a>
         </Link>
+      </div>
     </div>
-
   )
 }
 
-export default ProdBar;
+export default ProdBar
