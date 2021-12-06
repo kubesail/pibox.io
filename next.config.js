1ad33e245705734bf4a256f45c5777ffdb513e76
Dan Pastusek <dan@hexial.com>
2021-12-06 13:17:18 -0700

    remove extra hackersetup redirect

--- next.config.js
+++ next.config.js
@@ -7,11 +7,6 @@ module.exports = {
         permanent: false,
       },
       {
-        source: '/hackersetup',
-        destination: 'https://docs.kubesail.com/guides/pibox/rpiboot/',
-        permanent: false,
-      },
-      {
         source: '/case',
         destination: 'https://docs.kubesail.com/guides/pibox/hardware',
         permanent: false,
