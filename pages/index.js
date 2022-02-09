ee49f3ba409d77695f40c5d21045654dc65dd456
Dan Pastusek <dan@hexial.com>
2022-02-08 19:11:17 -0700

    add country headers

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout from '../components/Layout'
 import Home from '../components/Home'
+const { eeaMember, euMember } = require('is-european')
+const countryVat = require('country-vat')
 
 const Index = ({ country }) => (
   <Layout>
@@ -11,6 +13,11 @@ export default Index
 
 export async function getServerSideProps(context) {
   const country = context.req.headers['cf-ipcountry']
+  res.setHeader('x-your-country', country)
+  res.setHeader('x-eea-member', eeaMember(country))
+  res.setHeader('x-eu-member', euMember(country))
+  res.setHeader('x-your-vat-rate', countryVat(country))
+
   return {
     props: { country }, // will be passed to the page component as props
   }
